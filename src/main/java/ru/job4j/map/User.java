3a9137b5d41c70942ebb303aa3c339bd07b647a1
IDDQD <bigsobolev@yandex.ru>
2022-05-26 16:02:46 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,10 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class User {
 
@@ -8,9 +12,33 @@ public class User {
     private int children;
     private Calendar birthday;
 
+    @Override
+    public String toString() {
+        return "User{"
+                +
+                "name='" + name + '\''
+                +
+                ", children=" + children
+                +
+                ", birthday=" + birthday
+                +
+                '}';
+    }
+
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+
+    }
+
+    public static void main(String[] args) {
+        User userFirst = new User("Max", 2, new GregorianCalendar(1993, 7, 15));
+        User userSecond = new User("Max", 2, new GregorianCalendar(1993, 7, 15));
+        Map<User, Object> map = new HashMap<>();
+        map.put(userFirst, new Object());
+        map.put(userSecond, new Object());
+        BiConsumer<User, Object> consumer = (o1, o2) -> System.out.println("Key: " + o1 + "Value: " + o2);
+        map.forEach(consumer);
     }
 }
