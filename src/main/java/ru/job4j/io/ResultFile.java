016eb8a434b18e861ebff68ef6b1b3468bea398b
IDDQD <bigsobolev@yandex.ru>
2022-09-14 12:59:17 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        int size = 2;
+
+        try (FileOutputStream out = new FileOutputStream("rsl.txt")) {
+            for (int i = 0; i < size; i++) {
+                out.write(System.lineSeparator().getBytes());
+                for (int j = 0; j < size; j++) {
+                    String rsl = (i + 1) * (j + 1) + "\t";
+                    out.write(rsl.getBytes());
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
